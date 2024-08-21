f8ecf68194cd624e689bbfd06dcba887379b8fb9
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-22 00:00:16 +0800

    TensionLinearInterpolator

--- test/src/body-building/progress/TensionLinearInterpolator.cpp
+++ test/src/body-building/progress/TensionLinearInterpolator.cpp
@@ -1,1 +1,96 @@
 #include "TensionLinearInterpolator.h"
+#include <Option.h>
+
+void TensionLinearInterpolator::ChangeEndValue(int value)
+{
+    if (_linear_interpolator != nullptr)
+    {
+        _linear_interpolator = nullptr;
+        _current_output = value;
+        return;
+    }
+
+    if (value > _current_output)
+    {
+        double kg = static_cast<double>(_current_output) / 2 + 1.5;
+        if (kg < 15)
+        {
+            _linear_interpolator = std::shared_ptr<base::LinearInterpolator>{
+                new base::LinearInterpolator{
+                    _current_output,
+                    value,
+                    1000,
+                },
+            };
+        }
+        else if (kg < 25)
+        {
+            _linear_interpolator = std::shared_ptr<base::LinearInterpolator>{
+                new base::LinearInterpolator{
+                    _current_output,
+                    value,
+                    1500,
+                },
+            };
+        }
+        else
+        {
+            _linear_interpolator = std::shared_ptr<base::LinearInterpolator>{
+                new base::LinearInterpolator{
+                    _current_output,
+                    value,
+                    2500,
+                },
+            };
+        }
+    }
+    else
+    {
+        double dif = static_cast<double>(_current_output - value) / 2;
+        if (dif < 15)
+        {
+            _linear_interpolator = std::shared_ptr<base::LinearInterpolator>{
+                new base::LinearInterpolator{
+                    _current_output,
+                    value,
+                    500,
+                },
+            };
+        }
+        else
+        {
+            _linear_interpolator = std::shared_ptr<base::LinearInterpolator>{
+                new base::LinearInterpolator{
+                    _current_output,
+                    value,
+                    1000,
+                },
+            };
+        }
+    }
+}
+
+int TensionLinearInterpolator::StepForward()
+{
+    if (_linear_interpolator != nullptr)
+    {
+        (*_linear_interpolator)++;
+    }
+
+    return CurrentOutput();
+}
+
+int TensionLinearInterpolator::CurrentOutput()
+{
+    if (_linear_interpolator != nullptr)
+    {
+        _current_output = _linear_interpolator->CurrentValue();
+    }
+
+    if (_current_output < 4)
+    {
+        _current_output = 4;
+    }
+
+    return _current_output;
+}
--- test/src/body-building/progress/TensionLinearInterpolator.h
+++ test/src/body-building/progress/TensionLinearInterpolator.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <base/math/LinearInterpolator.h>
+#include <memory>
 
 /// @brief 拉力插值器。
 class TensionLinearInterpolator
@@ -6,10 +8,18 @@ class TensionLinearInterpolator
 private:
     TensionLinearInterpolator() = default;
 
+    std::shared_ptr<base::LinearInterpolator> _linear_interpolator = nullptr;
+    int _current_output = 0;
+
 public:
     static TensionLinearInterpolator &Instance()
     {
         static TensionLinearInterpolator o;
         return o;
     }
+
+    void ChangeEndValue(int value);
+
+    int StepForward();
+    int CurrentOutput();
 };
