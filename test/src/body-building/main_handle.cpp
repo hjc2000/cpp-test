19a9aa4b454a64ba04a34012966a723fed1d05c4
hjc2000 <hjc.666@foxmail.com>
2024-08-21 13:43:49 +0800

    void Refresh()

--- test/src/body-building/main_handle.cpp
+++ test/src/body-building/main_handle.cpp
@@ -3,14 +3,19 @@
 #include <Servo.h>
 #include <State.h>
 
+void Refresh()
+{
+    Servo::Instance().Refresh_FeedbackPosition();
+    Option::Instance().Refresh_AdditionalMode();
+    Option::Instance().Refresh_BodyBuildingMode();
+    Option::Instance().Refresh_Tension_kg();
+    State::Instance().Refresh_ReleasedLengthOfLine();
+}
+
 void main_handle()
 {
     while (true)
     {
-        Servo::Instance().Refresh_FeedbackPosition();
-        Option::Instance().Refresh_AdditionalMode();
-        Option::Instance().Refresh_BodyBuildingMode();
-        Option::Instance().Refresh_Tension_kg();
-        State::Instance().Refresh_ReleasedLengthOfLine();
+        Refresh();
     }
 }
