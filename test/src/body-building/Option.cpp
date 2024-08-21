8c8e17a9b242ab86cd1eb00e4e7a1e979bc46168
hjc2000 <hjc.666@foxmail.com>
2024-08-21 11:35:50 +0800

    class Option

--- test/src/body-building/Option.cpp
+++ test/src/body-building/Option.cpp
@@ -20,3 +20,75 @@ bool Option::BodyBuildingModeChanged()
 {
     return _body_building_mode != _last_body_building_mode;
 }
+
+void Option::Refresh_AdditionalMode()
+{
+    _last_additional_mode = _additional_mode;
+    // _additional_mode=
+}
+
+Option_AdditionalMode Option::AdditionalMode()
+{
+    return _additional_mode;
+}
+
+Option_AdditionalMode Option::LastAdditionalMode()
+{
+    return _last_additional_mode;
+}
+
+bool Option::AdditionalModeCodeChanged()
+{
+    return _additional_mode != _last_additional_mode;
+}
+
+void Option::Refresh_Tension_kg()
+{
+    _last_tension_kg = _tension_kg;
+    // _tension_kg=
+}
+
+double Option::Tension_kg()
+{
+    return _tension_kg;
+}
+
+double Option::LastTension_kg()
+{
+    return _last_tension_kg;
+}
+
+bool Option::Tension_kg_Changed()
+{
+    return _tension_kg != _last_tension_kg;
+}
+
+double Option::MaxTension_kg()
+{
+    return 0.0;
+}
+
+double Option::WindingSpeed()
+{
+    return 0.0;
+}
+
+double Option::TorqueRatio()
+{
+    return 1.85;
+}
+
+bool Option::UseGravitationPackage()
+{
+    return false;
+}
+
+int64_t Option::ZeroPositionProtectionThreshold()
+{
+    return 0;
+}
+
+int Option::UpdateFrequencyOfMaxTorque_Hz()
+{
+    return 0;
+}
--- test/src/body-building/Option.h
+++ test/src/body-building/Option.h
@@ -1,9 +1,15 @@
 #pragma once
+#include <stdint.h>
 
 /// @brief 健身模式
-enum Option_BodyBuildingMode
+enum class Option_BodyBuildingMode
 {
     Standard
+};
+
+enum class Option_AdditionalMode
+{
+    None,
 };
 
 class Option
@@ -12,9 +18,50 @@ private:
     Option_BodyBuildingMode _body_building_mode;
     Option_BodyBuildingMode _last_body_building_mode;
 
+    Option_AdditionalMode _additional_mode;
+    Option_AdditionalMode _last_additional_mode;
+
+    double _tension_kg = 0;
+    double _last_tension_kg = 0;
+
 public:
     void Refresh_BodyBuildingMode();
     Option_BodyBuildingMode BodyBuildingMode();
     Option_BodyBuildingMode LastBodyBuildingMode();
     bool BodyBuildingModeChanged();
+
+    void Refresh_AdditionalMode();
+    Option_AdditionalMode AdditionalMode();
+    Option_AdditionalMode LastAdditionalMode();
+    bool AdditionalModeCodeChanged();
+
+    void Refresh_Tension_kg();
+    double Tension_kg();
+    double LastTension_kg();
+    bool Tension_kg_Changed();
+
+    /// @brief 允许的最大拉力。
+    /// @return
+    double MaxTension_kg();
+
+    /// @brief 收线速度。
+    /// @return
+    double WindingSpeed();
+
+    /// @brief 转矩限制值 / 拉力_kg
+    /// @note 将 拉力_kg 乘上本属性，就可得到转矩限制值。转矩限制值是个百分比。
+    /// @return
+    double TorqueRatio();
+
+    /// @brief 使用引力包。
+    /// @return
+    bool UseGravitationPackage();
+
+    /// @brief 零点保护位置阈值。当伺服反馈位置的绝对值小于此值时需要保护措施。
+    /// @return
+    int64_t ZeroPositionProtectionThreshold();
+
+    /// @brief 最大转矩的更新频率。
+    /// @return
+    int UpdateFrequencyOfMaxTorque_Hz();
 };
