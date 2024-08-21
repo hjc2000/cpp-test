f887da02d37458c432015cb2247726421f4d606b
hjc2000 <hjc.666@foxmail.com>
2024-08-21 13:35:56 +0800

    void main_handle()

--- test/src/body-building/main_handle.cpp
+++ test/src/body-building/main_handle.cpp
@@ -1,1 +1,16 @@
 #include "main_handle.h"
+#include <Option.h>
+#include <Servo.h>
+#include <State.h>
+
+void main_handle()
+{
+    while (true)
+    {
+        Servo::Instance().Refresh_FeedbackPosition();
+        Option::Instance().Refresh_AdditionalMode();
+        Option::Instance().Refresh_BodyBuildingMode();
+        Option::Instance().Refresh_Tension_kg();
+        State::Instance().Refresh_ReleasedLengthOfLine();
+    }
+}
--- test/src/body-building/main_handle.h
+++ test/src/body-building/main_handle.h
@@ -1,16 +1,3 @@
 #pragma once
-#include <Option.h>
-#include <Servo.h>
-#include <State.h>
 
-void main_handle()
-{
-    while (true)
-    {
-        Servo::Instance().Refresh_FeedbackPosition();
-        Option::Instance().Refresh_AdditionalMode();
-        Option::Instance().Refresh_BodyBuildingMode();
-        Option::Instance().Refresh_Tension_kg();
-        State::Instance().Refresh_ReleasedLengthOfLine();
-    }
-}
+void main_handle();
