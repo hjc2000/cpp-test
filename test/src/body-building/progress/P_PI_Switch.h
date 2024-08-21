b0610af818c1097638a66f7b9aa5a845238f845e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-21 23:31:28 +0800

    P_PI_Switch

--- test/src/body-building/progress/P_PI_Switch.h
+++ test/src/body-building/progress/P_PI_Switch.h
@@ -2,6 +2,15 @@
 
 class P_PI_Switch
 {
+private:
+    P_PI_Switch() = default;
+
 public:
+    static P_PI_Switch &Instance()
+    {
+        static P_PI_Switch o;
+        return o;
+    }
+
     void Execute();
 };
