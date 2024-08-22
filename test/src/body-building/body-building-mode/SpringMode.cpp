ed4441f141e644241016e0c6387a91082025c659
hjc2000 <hjc.666@foxmail.com>
2024-08-22 17:06:07 +0800

    SpringMode

--- test/src/body-building/body-building-mode/SpringMode.cpp
+++ test/src/body-building/body-building-mode/SpringMode.cpp
@@ -1,5 +1,43 @@
 #include "SpringMode.h"
+#include <Cmd.h>
+#include <Option.h>
+#include <Servo.h>
+#include <TensionLinearInterpolator.h>
 
 void SpringMode::Execute()
 {
+    Cmd::Instance().SetSpeed(Option::Instance().WindingSpeed());
+
+    if (Option::Instance().BodyBuildingModeChanged() || Option::Instance().Tension_kg_Changed())
+    {
+        TensionLinearInterpolator::Instance().ChangeEndValue(Option::Instance().Tension_kg());
+
+        _filter = std::shared_ptr<base::InertialElement>{
+            new base::InertialElement{
+                base::InertialElement_TimeConstant{0.025},
+                base::InertialElement_SampleInterval{0.002},
+            },
+        };
+    }
+
+    double tension = TensionLinearInterpolator::Instance().StepForward();
+    double torque = tension * Option::Instance().TorqueRatio();
+
+    if (Servo::Instance().FeedbackPosition() < OneMeterPosition())
+    {
+        torque = torque * Servo::Instance().FeedbackPosition() / OneMeterPosition();
+    }
+    else
+    {
+        int delta_position = Servo::Instance().FeedbackPosition() - OneMeterPosition();
+        torque = torque + (delta_position * Ratio() / 30000);
+    }
+
+    torque = _filter->Input(torque);
+    if (torque < 5)
+    {
+        torque = 5;
+    }
+
+    Cmd::Instance().SetTorque(torque);
 }
--- test/src/body-building/body-building-mode/SpringMode.h
+++ test/src/body-building/body-building-mode/SpringMode.h
@@ -1,9 +1,33 @@
 #pragma once
+#include <base/math/InertialElement.h>
+#include <memory>
 
+/// @brief 弹簧模式
 class SpringMode
 {
 private:
     SpringMode() = default;
+
+    /// @brief 放线 1m 对应的伺服反馈位置。
+    /// @return
+    int OneMeterPosition()
+    {
+        return 10000;
+    }
+
+    /// @brief 弹簧弹性系数。
+    /// @return
+    double Ratio()
+    {
+        return 1.5;
+    }
+
+    std::shared_ptr<base::InertialElement> _filter{
+        new base::InertialElement{
+            base::InertialElement_TimeConstant{0.025},
+            base::InertialElement_SampleInterval{0.002},
+        },
+    };
 
 public:
     static SpringMode &Instance()
