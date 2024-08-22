ba949cb487dd4634109d2d80e9fe5ffc1c4c0318
hjc2000 <hjc.666@foxmail.com>
2024-08-22 14:09:39 +0800

    FrictionTorqueCalculater

--- test/src/body-building/progress/FrictionTorqueCalculater.cpp
+++ test/src/body-building/progress/FrictionTorqueCalculater.cpp
@@ -18,32 +18,32 @@ int FrictionTorqueCalculater::UnwindingFrictionTorque(double tension_kg) const
 int FrictionTorqueCalculater::Calculate(double tension_kg)
 {
     double mcl = 2;
-    int torque = 0;
     if (Servo::Instance().FeedbackSpeed() >= 0 && Servo::Instance().FeedbackSpeed() < mcl)
     {
         // 低速放线
         double base = UnwindingFrictionTorque(tension_kg);
         double ratio = std::abs(Servo::Instance().FeedbackSpeed()) / mcl;
-        torque = base * ratio;
+        _current_friction_torque = base * ratio;
     }
     else if (Servo::Instance().FeedbackSpeed() < 0 && Servo::Instance().FeedbackSpeed() > -mcl)
     {
         // 低速收线
         double base = WindingFrictionTorque();
         double ratio = std::abs(Servo::Instance().FeedbackSpeed()) / mcl;
-        torque = base * ratio;
+        _current_friction_torque = base * ratio;
     }
     else if (Servo::Instance().FeedbackSpeed() > mcl)
     {
         // 高速放线
-        torque = UnwindingFrictionTorque(tension_kg);
+        _current_friction_torque = UnwindingFrictionTorque(tension_kg);
     }
     else
     {
         // 高速收线
-        torque = WindingFrictionTorque();
+        _current_friction_torque = WindingFrictionTorque();
     }
 
-    torque = static_cast<int>(_filter.Input(torque));
-    return torque;
+    _current_friction_torque = static_cast<int>(_filter.Input(_current_friction_torque));
+    _last_friction_torque = _current_friction_torque;
+    return _current_friction_torque;
 }
