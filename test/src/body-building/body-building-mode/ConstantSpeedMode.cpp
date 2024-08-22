a92afeffefb9879094030ead59d529d3c6f6ae15
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-22 23:43:56 +0800

    ConstantSpeedMode

--- test/src/body-building/body-building-mode/ConstantSpeedMode.cpp
+++ test/src/body-building/body-building-mode/ConstantSpeedMode.cpp
@@ -1,4 +1,148 @@
 #include "ConstantSpeedMode.h"
+#include <Cmd.h>
+#include <Option.h>
+#include <Servo.h>
+#include <TensionLinearInterpolator.h>
+
+double ConstantSpeedMode::TorqueGrowRatio()
+{
+    switch (Option::Instance().BodyBuildingMode())
+    {
+    case Option_BodyBuildingMode::ConstantSpeedMode1:
+        {
+            return SRV_PARA(1, 39) / 1000;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode2:
+        {
+            return 1;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode3:
+        {
+            return 1.45;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode4:
+        {
+            return 2;
+        }
+    default:
+    case Option_BodyBuildingMode::ConstantSpeedMode5:
+        {
+            return 0.6;
+        }
+    }
+}
+
+double ConstantSpeedMode::SpeedLimit()
+{
+    switch (Option::Instance().BodyBuildingMode())
+    {
+    case Option_BodyBuildingMode::ConstantSpeedMode1:
+        {
+            return SRV_PARA(1, 21) / 100;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode2:
+        {
+            return 1400;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode3:
+        {
+            return 1100;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode4:
+        {
+            return 1;
+        }
+    default:
+    case Option_BodyBuildingMode::ConstantSpeedMode5:
+        {
+            return 1800;
+        }
+    }
+}
+
+int ConstantSpeedMode::Torque()
+{
+    switch (Option::Instance().BodyBuildingMode())
+    {
+    case Option_BodyBuildingMode::ConstantSpeedMode1:
+        {
+            return SRV_PARA(2, 18);
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode2:
+        {
+            return 13;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode3:
+        {
+            return 30;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode4:
+        {
+            return 45;
+        }
+    default:
+    case Option_BodyBuildingMode::ConstantSpeedMode5:
+        {
+            return 6;
+        }
+    }
+}
+
+double ConstantSpeedMode::WindingSpeed()
+{
+    switch (Option::Instance().BodyBuildingMode())
+    {
+    case Option_BodyBuildingMode::ConstantSpeedMode1:
+        {
+            return SRV_PARA(1, 10) / 100;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode2:
+        {
+            return 600;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode3:
+        {
+            return 400;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode4:
+        {
+            return 300;
+        }
+    default:
+    case Option_BodyBuildingMode::ConstantSpeedMode5:
+        {
+            return 800;
+        }
+    }
+}
+
+int ConstantSpeedMode::FilterGain()
+{
+    switch (Option::Instance().BodyBuildingMode())
+    {
+    case Option_BodyBuildingMode::ConstantSpeedMode1:
+        {
+            return SRV_PARA(1, 20);
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode2:
+        {
+            return 200;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode3:
+        {
+            return 200;
+        }
+    case Option_BodyBuildingMode::ConstantSpeedMode4:
+        {
+            return 1000;
+        }
+    default:
+    case Option_BodyBuildingMode::ConstantSpeedMode5:
+        {
+            return 500;
+        }
+    }
+}
 
 void ConstantSpeedMode::Execute()
 {
--- test/src/body-building/body-building-mode/ConstantSpeedMode.h
+++ test/src/body-building/body-building-mode/ConstantSpeedMode.h
@@ -8,31 +8,23 @@ private:
 
     /// @brief 限制转速时让转矩增长的比例。越大转矩随着速度变大增长越快。
     /// @return
-    double TorqueGrowRatio()
-    {
-        return SRV_PARA(1, 39) / 1000;
-    }
+    double TorqueGrowRatio();
 
     /// @brief 转速限制值。超过后转矩会快速增大。
     /// @return
-    double SpeedLimit()
-    {
-        return SRV_PARA(1, 21) / 100;
-    }
+    double SpeedLimit();
 
     /// @brief 恒速模式的收线转矩。
     /// @return
-    int Torque()
-    {
-        return SRV_PARA(2, 18);
-    }
+    int Torque();
 
     /// @brief 恒速模式下的收线转速。
     /// @return
-    double WindingSpeed()
-    {
-        return SRV_PARA(1, 10) / 100;
-    }
+    double WindingSpeed();
+
+    /// @brief ChX 滤波器的前向通道增益
+    /// @return
+    int FilterGain();
 
 public:
     static ConstantSpeedMode &Instance()
