8b6cf05d80e6584f26ee806c1ef101d878d88d7d
hjc2000 <hjc.666@foxmail.com>
2024-08-22 16:05:02 +0800

    CentrifugalMode

--- test/src/body-building/body-building-mode/CentrifugalMode.cpp
+++ test/src/body-building/body-building-mode/CentrifugalMode.cpp
@@ -1,5 +1,32 @@
 #include "CentrifugalMode.h"
+#include <Cmd.h>
+#include <Option.h>
+#include <Servo.h>
+#include <TensionLinearInterpolator.h>
 
 void CentrifugalMode::Execute()
 {
+    Cmd::Instance().SetSpeed(Option::Instance().WindingSpeed());
+
+    if (Option::Instance().BodyBuildingModeChanged() || Option::Instance().Tension_kg_Changed())
+    {
+        TensionLinearInterpolator::Instance().ChangeEndValue(Option::Instance().Tension_kg());
+
+        _filter = std::shared_ptr<base::InertialElement>{
+            new base::InertialElement{
+                base::InertialElement_TimeConstant{0.025},
+                base::InertialElement_SampleInterval{0.002},
+            },
+        };
+    }
+
+    double tension = TensionLinearInterpolator::Instance().StepForward();
+    double torque = tension * Option::Instance().TorqueRatio();
+
+    if (Servo::Instance().FeedbackSpeed() > -50)
+    {
+        torque = torque / Ratio();
+    }
+
+    Cmd::Instance().SetTorque(torque);
 }
--- test/src/body-building/body-building-mode/CentrifugalMode.h
+++ test/src/body-building/body-building-mode/CentrifugalMode.h
@@ -1,10 +1,27 @@
 #pragma once
+#include <base/math/InertialElement.h>
+#include <memory>
 
 /// @brief 离心模式
 class CentrifugalMode
 {
 private:
     CentrifugalMode() = default;
+
+    /// @brief 大于 1 的值。
+    /// @note 离心模式下，顺从它离心，拉力会减小。减小拉力是通过将转矩除以本系数。
+    /// @return
+    double Ratio()
+    {
+        return 2;
+    }
+
+    std::shared_ptr<base::InertialElement> _filter{
+        new base::InertialElement{
+            base::InertialElement_TimeConstant{0.025},
+            base::InertialElement_SampleInterval{0.002},
+        },
+    };
 
 public:
     static CentrifugalMode &Instance()
