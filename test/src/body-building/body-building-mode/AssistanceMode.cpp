1c2454efb9e5d834b7bce67f34005992452a0346
hjc2000 <hjc.666@foxmail.com>
2024-08-23 13:38:58 +0800

    AssistanceMode

--- test/src/body-building/body-building-mode/AssistanceMode.cpp
+++ test/src/body-building/body-building-mode/AssistanceMode.cpp
@@ -1,4 +1,15 @@
 #include "AssistanceMode.h"
+#include <base/math/InertialElement.h>
+#include <ChXFilter.h>
+#include <Cmd.h>
+#include <DirectionDetector.h>
+#include <lua_api.h>
+#include <memory>
+#include <Option.h>
+#include <PullLengthDetecter.h>
+#include <PullTimesDetector.h>
+#include <Servo.h>
+#include <TensionLinearInterpolator.h>
 
 void AssistanceMode::Execute()
 {
