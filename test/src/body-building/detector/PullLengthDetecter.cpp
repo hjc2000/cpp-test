267f07daf9f42990e8f5bc1813c7d0af686ff17f
hjc2000 <hjc.666@foxmail.com>
2024-08-23 09:56:36 +0800

    PullLengthDetecter

--- test/src/body-building/detector/PullLengthDetecter.cpp
+++ test/src/body-building/detector/PullLengthDetecter.cpp
@@ -1,5 +1,35 @@
 #include "PullLengthDetecter.h"
+#include <DirectionDetector.h>
+#include <PullTimesDetector.h>
 
 void PullLengthDetecter::Execute()
 {
+    if (PullTimesDetector::Instance().UnwindingTimesChanged())
+    {
+        // 经历了有效出绳
+        _has_effective_unwinding = true;
+    }
+    else if (PullTimesDetector::Instance().WindingTimesChanged())
+    {
+        // 经历了有效收绳
+        _has_effective_winding = true;
+        _max_length = 0;
+    }
+
+    if (_has_effective_unwinding &&
+        DirectionDetector::Instance().DirectionChange() == DirectionDetector_DirectionChange::FromUnwindingToWinding)
+    {
+        // 经历了有效出绳，且现在方向从出绳变成收绳了
+        _has_effective_unwinding = false;
+
+        // 方向切换瞬间的绳长就是本轮拉绳的最大长度
+        _max_length = DirectionDetector::Instance().ReleasedLengthOfLineWhenDirectionChanged();
+    }
+    else if (_has_effective_winding &&
+             DirectionDetector::Instance().DirectionChange() == DirectionDetector_DirectionChange::FromWindingToUnwinding)
+    {
+        // 经历了有效收线，现在方向变成放线了
+        _has_effective_winding = false;
+        _min_length = DirectionDetector::Instance().ReleasedLengthOfLineWhenDirectionChanged();
+    }
 }
--- test/src/body-building/detector/PullLengthDetecter.h
+++ test/src/body-building/detector/PullLengthDetecter.h
@@ -6,6 +6,11 @@ class PullLengthDetecter
 private:
     PullLengthDetecter() = default;
 
+    int _min_length = 0;
+    int _max_length = 0;
+    bool _has_effective_unwinding = false;
+    bool _has_effective_winding = false;
+
 public:
     static PullLengthDetecter &Instance()
     {
@@ -14,4 +19,17 @@ public:
     }
 
     void Execute();
+
+    /// @brief 本轮拉绳绳子被拉出的长度
+    /// @return
+    int PullLength()
+    {
+        int length = _max_length - _min_length;
+        if (length < 0)
+        {
+            length = 0;
+        }
+
+        return length;
+    }
 };
