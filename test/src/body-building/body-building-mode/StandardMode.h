a160f7c1b32f28872a7a146c56da09ad772cc7b3
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-24 21:21:41 +0800

    _tension_linear_interpolator

--- test/src/body-building/body-building-mode/StandardMode.h
+++ test/src/body-building/body-building-mode/StandardMode.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <base/IExecutable.h>
+#include <base/math/LinearInterpolator.h>
+#include <Option.h>
 
 class StandardMode :
     public base::IExecutable
 {
+private:
+    base::LinearInterpolator _tension_linear_interpolator{
+        0,
+        Option::Instance().Tension_kg(),
+        base::LinearInterpolator_StepLength{1.5},
+    };
+
 public:
     void Execute();
 };
