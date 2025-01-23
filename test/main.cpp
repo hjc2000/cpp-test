27dbea27b2dc3975704fd7908529e4026aa00369
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-24 00:16:37 +0800

    target_link_libraries(${ProjectName} PUBLIC -Wl,--start-group)

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,3 @@
-#include "Poco/Delegate.h"
-#include "Poco/Net/Net.h"
 #include <algorithm>
 #include <base/container/List.h>
 #include <base/di/SingletonGetter.h>
@@ -9,10 +7,6 @@
 #include <ffmpeg-wrapper/mux/SptsEncodeMux.h>
 #include <filesystem>
 #include <iostream>
-#include <Poco/DateTime.h>
-#include <Poco/DateTimeFormatter.h>
-#include <Poco/Net/NTPClient.h>
-#include <Poco/Timespan.h>
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDebug>
 #include <sdl2-wrapper/AVPacketPlayer.h>
@@ -24,62 +18,26 @@
 
 void LogBuffer();
 
-class MyEventHandler
-{
-private:
-	Poco::Timestamp T1 = Poco::Timestamp(); // Originate Time
-
-public:
-	// 定义事件处理器方法
-	void onNTPResponse(void const *sender, Poco::Net::NTPEventArgs &args)
-	{
-		Poco::Timestamp T2 = args.packet().receiveTime();  // Receive Time
-		Poco::Timestamp T3 = args.packet().transmitTime(); // Transmit Time
-		Poco::Timestamp T4 = Poco::Timestamp();            // Current client time when receiving the response
-		Poco::Timespan delay = ((T4 - T1) - (T3 - T2)) / 2;
-		Poco::Timespan offset = ((T2 - T1) + (T3 - T4)) / 2;
-		Poco::Timestamp accurateTime = T4 + offset + Poco::Timespan(0, 8, 0, 0, 0);
-
-		// 定义格式字符串
-		std::string format = "%Y-%m-%d %H:%M:%S";
-
-		// 使用 Poco::DateTimeFormatter 格式化时间
-		std::string formattedTime = Poco::DateTimeFormatter::format(accurateTime, format);
-
-		// 输出格式化后的时间
-		std::cout << "Formatted Time: " << formattedTime << std::endl;
-	}
-};
-
 int main()
 {
-	Poco::Net::NTPClient client{Poco::Net::SocketAddress::Family::IPv4};
-	MyEventHandler eventHandler;
-	client.response += Poco::delegate(&eventHandler, &MyEventHandler::onNTPResponse);
-	// client.request("ntp.ntsc.ac.cn");
-	client.request("time.windows.com");
+	try
+	{
+		// MessageBox(NULL, "Hello world!", "标题", MB_OK);
+		// return 0;
+
+		std::filesystem::current_path(Predefine_ResourceDir);
+		video::test_AVPacketPlayer();
+
+		// test_SptsEncodeMux();
+		// test_tsduck();
+		return 0;
+	}
+	catch (std::runtime_error const &e)
+	{
+		std::cout << e.what() << std::endl;
+		throw;
+	}
 }
-
-// int main()
-// {
-// 	try
-// 	{
-// 		// MessageBox(NULL, "Hello world!", "标题", MB_OK);
-// 		// return 0;
-
-// 		std::filesystem::current_path(Predefine_ResourceDir);
-// 		video::test_AVPacketPlayer();
-
-// 		// test_SptsEncodeMux();
-// 		// test_tsduck();
-// 		return 0;
-// 	}
-// 	catch (std::runtime_error const &e)
-// 	{
-// 		std::cout << e.what() << std::endl;
-// 		throw;
-// 	}
-// }
 
 // int main()
 //{
