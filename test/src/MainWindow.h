4d2d3a04c49d16571ceb4d492c0dcd28de04669b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-24 23:03:42 +0800

    MainWindow

--- test/src/MainWindow.h
+++ test/src/MainWindow.h
@@ -19,10 +19,10 @@ namespace test
 			setWindowTitle("带有侧边栏的 Hello World");
 
 			// 创建一个中央部件
-			QWidget *centralWidget = new QWidget(this);
+			QWidget *centralWidget = new QWidget{this};
 
 			// 创建 QVBoxLayout 实例并将其设置为中央部件的布局
-			QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+			QVBoxLayout *layout = new QVBoxLayout{centralWidget};
 			layout->setAlignment(Qt::Alignment{Qt::AlignmentFlag::AlignTop});
 
 			// 添加一些按钮到布局中作为示例
